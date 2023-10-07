5c58d56f34d3ff5c5a5f37efcbeaa70ed3a0f96d
Yuxuan <liyux@seas.upenn.edu>
2023-10-07 15:22:45 -0400

    The first initialization and test of the trade-dev project

---
+++ trade-goods/src/main/java/com/shangan/tradegoods/GoodsController.java
@@ -0,0 +1,16 @@
+package com.shangan.tradegoods;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Slf4j
+@Controller
+public class GoodsController {
+    @RequestMapping("/goods/test")
+    @ResponseBody
+    public String test() {
+        return "hello world java!";
+    }
+}
---
+++ trade-goods/src/main/java/com/shangan/tradegoods/TradeGoodsApplication.java
@@ -0,0 +1,13 @@
+package com.shangan.tradegoods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeGoodsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeGoodsApplication.class, args);
+    }
+
+}
