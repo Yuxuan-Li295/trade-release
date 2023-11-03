d0f9f7221e3294336cacf04348e75061daa71392
Yuxuan <liyux@seas.upenn.edu>
2023-11-03 03:47:22 -0400

    Development of Dealy Message with RabbitMQ

--- trade-order/src/main/java/com/shangan/tradeorder/TradeOrderApplication.java
+++ trade-order/src/main/java/com/shangan/tradeorder/TradeOrderApplication.java
@@ -2,10 +2,12 @@ package com.shangan.tradeorder;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableRabbit
 @ComponentScan(basePackages = {"com.shangan"})
 @MapperScan({"com.shangan.tradeorder.db.mappers","com.shangan.tradegoods.db.mappers"})
 @SpringBootApplication
