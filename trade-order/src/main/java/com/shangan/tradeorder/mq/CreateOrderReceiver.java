2a889f66051605a2a3150d9ce53b794f1be20117
Yuxuan <liyux@seas.upenn.edu>
2023-11-10 16:40:36 -0500

    Asynchronisation of the falsh orders

---
+++ trade-order/src/main/java/com/shangan/tradeorder/mq/CreateOrderReceiver.java
@@ -0,0 +1,32 @@
+package com.shangan.tradeorder.mq;
+
+import com.alibaba.fastjson.JSON;
+import com.shangan.tradeorder.db.dao.OrderDao;
+import lombok.extern.slf4j.Slf4j;
+import com.shangan.tradeorder.db.model.Order;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class CreateOrderReceiver {
+    @Autowired
+    private OrderDao orderDao;
+
+    @Autowired
+    private OrderMessageSender orderMessageSender;
+
+    //Process the create order message
+    @RabbitListener(queues = "create.order.queue")
+    public void process(String message) {
+        log.info("创建订单消息处理，接收到消息内容:{}", message);
+        Order order = JSON.parseObject(message, Order.class);
+        boolean insertResult = orderDao.insertOrder(order);
+        if (!insertResult) {
+            log.error("order insert error order={}", JSON.toJSONString(order));
+            throw new RuntimeException("订单生成失败");
+        }
+        orderMessageSender.sendPayStatusCheckDelayMessage(JSON.toJSONString(order));
+    }
+}
